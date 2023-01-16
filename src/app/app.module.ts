bf84f5ff16a3b19ae6566a9276cb153a0d4f9929
Betinsk <gmdust@htomail.com>
2023-01-16 12:43:37 -0300

    nav-phonomena

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavComponent } from './nav/nav.component';
+import { NavPhenomenaComponent } from './nav-phenomena/nav-phenomena.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavComponent
+    NavComponent,
+    NavPhenomenaComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/nav-phenomena/nav-phenomena.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-phenomena',
+  templateUrl: './nav-phenomena.component.html',
+  styleUrls: ['./nav-phenomena.component.css']
+})
+export class NavPhenomenaComponent {
+
+}
